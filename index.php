ddaca2ca5e88a573b094033f571169183c3f5b63
Affan Mansoor <affanmansoor630@gmail.com>
2023-12-11 00:15:55 +0500

    card added

--- index.php
+++ index.php
@@ -90,38 +90,41 @@
 		</div>
 	</section>
 			<!-- slider -->
+	<!-- <br> -->
+	<!--cards start-->
+	<h1  style="text-align:center;">BEST LAWYERS</style></h1>
 
-	<br>
-	<br>
-	<!-- <section class="aboutus">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-6 ml-auto">
-					<h1>About Us</h1>
-					<p>Welcome to our Lawyer Management System, where we put you in control. Our platform is all about
-						making the legal process as effortless as possible. We've created a space where you, the user,
-						can browse, choose, and book appointments with lawyers who best suit your unique needs. It's
-						your legal journey, your way, and we're here to make it seamless. Trust us to connect you with
-						the right legal expertise when you need it most.</p>
+	<div class="card-deck">
+  <div class="card">
+    <img class="baner.jpg" src="images/upload/20231028142702_2.jpg" alt="baner.jpg">
+    <div class="card-body">
+      <h4 class="card-title" style="font-weight:bold;">Basit Khan</h4>
+	  <h5 class="card-text">Religious Law</h5>
+      <p class="card-text"><p class="card-text" style="font-weight: bold">Type of case handles:</p>Civil matter,Contract law,Construction law,Information Technology,Family Law,Investment Matter,Property Law</p>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="images/upload/20231028143018_3.jpg" alt="Card image cap">
+    <div class="card-body">
+	 <h4 class="card-title" style="font-weight:bold;">Aqsa Jamil</h4>
+      <h5 class="card-text">Family Law</h5>
+      <p class="card-text" style="font-weight: bold">Type of case handles:</p><p class="card-text"> Criminal matter,Civil matter,Writ Jurisdiction,Company law,Contract law,Commercial matter,Construction law,Information Technology,Family Law,Religious Matter,Investment Matter,Labour Law,Property Law,Taxation Matter,Others,</p>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="images/upload/20231028142837_1.jpg" alt="Card image cap">
+    <div class="card-body">
+	 <h4 class="card-title" style="font-weight:bold;">Basit Khan</h4>
+      <h5 class="card-text">Investment Law</h5>
+      <p class="card-text"><p class="card-text" style="font-weight: bold">Type of case handles:</p>Criminal matter,Civil matter,Writ Jurisdiction,Company law,Contract law,Commercial matter,Construction law,Information Technology,Family Law,Religious Matter,Investment Matter,Labour Law,Property Law,Taxation Matter,Others,</p>
+    </div>
+  </div>
+</div>
 
-				</div>
-				<div class="col-md-6">
-					<div class="contact-us-bar">
-						<h1 class="contact-header">Get in Touch</h1>
-						<div class="contact-details">
-							<p><i class="fas fa-map-marker-alt"></i> Address: D-18, Block-4</p>
-							<p><i class="fas fa-phone"></i> Phone: +9234-332-08987</p>
-							<p><i class="fas fa-envelope"></i> Email: lawyers@contact.com</p>
-							<p><i class="fas fa-headset"></i> Customer Support: 021-58585-3</p>
-							<p><i class="far fa-clock"></i> Office Hours: 9am-4pm</p>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section> -->
-	<br>
-	<br>
+<!---cards end-->
+
+	<link rel="stylesheet" href=
+	         
 	<!-- FOOTER STARTS  -->
 	<footer style="background-color: #28a745;">
 		<div class="container p-4">
